3f2bda10c7a45361aa103c3137bff13e6f15f15f
azuraopal <muhammadnovals334@gmail.com>
2024-10-05 10:56:35 +0700

    feat: add user role

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'full_name',
         'email',
         'password',
+        'description',
+        'image',
     ];
 
     /**
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use function Pest\Laravel\withMiddleware;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -14,9 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+        ])
+        ->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
 
-        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
