58b9c5bba53dc118301d4214ea99ede0b3bfb02c
Dyzean <ashtrath@pm.me>
2024-10-07 15:34:51 +0700

    feat: add Kegiatan logic

---
+++ app/Enums/KegiatanStatus.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum KegiatanStatus: string
+{
+    case Terbit = 'Terbit';
+    case Draft = 'Draft';
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\KegiatanController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SektorController;
+use App\Http\Controllers\TagController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +25,31 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware('role:Admin')->group(function() {
+        Route::get('sektor/{sektor}', [SektorController::class, 'show'])->name('sektor.show');
+        Route::get('sektor/create', [SektorController::class, 'create'])->name('sektor.create');
+        Route::post('sektor', [SektorController::class, 'store'])->name('sektor.store');  
+        Route::put('sektor/{sektor}', [SektorController::class, 'update'])->name('sektor.update');
+        Route::get('sektor/{sektor}/edit', [SektorController::class, 'edit'])->name('sektor.edit');
+        Route::delete('sektor/{sektor}', [SektorController::class, 'destroy'])->name('sektor.destroy');
+        
+        Route::get('kegiatan/{kegiatan}', [KegiatanController::class, 'show'])->name('kegiatan.show');
+        Route::get('kegiatan/create', [KegiatanController::class, 'create'])->name('kegiatan.create');
+        Route::post('kegiatan', [KegiatanController::class, 'store'])->name('kegiatan.store');
+        Route::get('kegiatan/{kegiatan}/edit', [KegiatanController::class, 'edit'])->name('kegiatan.edit');
+        Route::put('kegiatan/{kegiatan}', [KegiatanController::class, 'update'])->name('kegiatan.update');
+        Route::delete('kegiatan/{kegiatan}', [KegiatanController::class, 'destroy'])->name('kegiatan.destroy');
+    });
+
+    // sektor 
+    Route::get('sektor', [SektorController::class, 'index'])->name('sektor.index');
+    Route::get('sektor/{sektor}', [SektorController::class, 'show'])->name('sektor.show');
+
+    // kegiatan
+    Route::get('kegiatan', [KegiatanController::class, 'index'])->name('kegiatan.index');
+    Route::get('kegiatan/{kegiatan}', [KegiatanController::class, 'show'])->name('kegiatan.show');
+
 });
 
 require __DIR__.'/auth.php';
