21dc3719a509fadb3c0a25366425f0f06e0aa186
azuraopal <muhammadnovals334@gmail.com>
2024-10-08 09:09:23 +0700

    feat: ProjectController and add projects routes

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,89 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
+use App\Models\Sektor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Inertia\Inertia;
 
 class ProjectController extends Controller
 {
-    
+    public function index()
+    {
+        $projects = Project::with('sektor')->get();
+        return Inertia::render('Project/Index', ['project' => $projects]);
+    }
+
+    public function create()
+    {
+        $sektors = Sektor::all();
+        return Inertia::render('Project/Create', ['sektor' => $sektors]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'image' => 'required|string|max:255',
+            'tanggal_awal' => 'required|date',
+            'tanggal_akhir' => 'required|date|after:tangal_awal',
+            'sektor_id' => 'required|exists:sektors.id',
+        ]);
+
+        $imagePath = $request->file('image')->store('image_project', 'public');
+
+        Project::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $imagePath,
+            'tanggal_awal' => $request->tanggal_awal,
+            'tanggal_akhir' => $request->tanggal_akhir,
+            'sektor_id' => $request->sektor_id,
+        ]);
+
+        return redirect()->route('project.index')->with('success', 'Project Berhasil Dibuat.');
+    }
+
+    public function edit(Project $project)
+    {
+        $sektors = Sektor::all();
+        return Inertia::render('Project/Edit', [
+            'project' => $project,
+            'sektors' => $sektors
+        ]);
+    }
+
+    public function update(Request $request, Project $project)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'image' => 'required|string|max:255',
+            'tanggal_awal' => 'required|date',
+            'tanggal_akhir' => 'required|date|after:tangal_awal',
+            'sektor_id' => 'required|exists:sektors.id',
+        ]);
+
+        $data = $request->except('image');
+
+        if ($request->hasFile('image')) {
+            Storage::disk('public')->delete($project->image);
+            $imagePath = $request->file('image')->store('image_project', 'public');
+            $data['image'] = $imagePath;
+        }
+
+        $project->update($data);
+
+        return redirect()->route('project.index')->with('success', 'Project Berhasil Diperbaharui.');
+    }
+
+    public function destroy(Project $project)
+    {
+        Storage::disk('public')->delete($project->image);
+        $project->delete();
+
+        return redirect()->route('project.index')->with('success', 'Project Berhasil Dihapus');
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
@@ -20,8 +21,13 @@ class Project extends Model
         'sektor_id',
 
     ];
+    
+    protected $casts = [
+        'tanggal_awal' => 'date',
+        'tanggal_akhir' => 'date'
+    ];
 
-    public function sektor()
+    public function sektor(): BelongsTo
     {
         return $this->belongsTo(Sektor::class, 'sektor_id');
     }
