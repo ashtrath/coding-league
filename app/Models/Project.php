2b93c4e96485d4cb6770d3ee7ce3d8ee17d3876f
azuraopal <muhammadnovals334@gmail.com>
2024-10-08 08:23:01 +0700

    feat: Project and change routes

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'lokasi_kecamatan',
+        'tanggal_awal',
+        'tanggal_akhir',
+        'tanggal_diterbitkan',
+        'sektor_id',
+
+    ];
+
+    public function sektor()
+    {
+        return $this->belongsTo(Sektor::class, 'sektor_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
