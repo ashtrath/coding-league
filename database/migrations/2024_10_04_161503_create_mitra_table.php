71579e90812b68fbcb91f7bca37fa7564cecf406
Rahadian Fahri Faizani <69681505+ashtrath@users.noreply.github.com>
2024-10-08 15:48:42 +0700

    feat: change mitra table name

--- database/migrations/2024_10_04_161503_create_mitra_table.php
+++ database/migrations/2024_10_04_161503_create_mitra_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('mitra', function (Blueprint $table) {
+        Schema::create('mitras', function (Blueprint $table) {
             $table->ulid('id')->primary();
             $table->foreignUlid('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('name_company');
-            $table->string('phone_number', 20);
+            $table->string('phone_number', 20)->nullable();
             $table->string('address')->nullable();
             $table->enum('status', array_column(MitraStatus::cases(), 'value'))->default(MitraStatus::Active->value);
             $table->timestamps();
