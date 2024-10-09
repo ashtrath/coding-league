874c6c69ffafdbc3ea0824155a6e9b5c1ca0ebec
Rahadian Fahri Faizani <69681505+ashtrath@users.noreply.github.com>
2024-10-09 11:25:28 +0700

    chore(Button): update style (again)

--- resources/js/Components/UI/Button/index.tsx
+++ resources/js/Components/UI/Button/index.tsx
@@ -5,13 +5,13 @@ import { cn } from '@/lib/utils';
 import { forwardRef } from 'react';
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center whitespace-nowrap rounded-md text-base font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+    'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-base font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
     {
         variants: {
             variant: {
-                default: 'bg-brand text-white hover:bg-brand/90',
+                default: 'bg-brand text-white hover:bg-brand/90 shadow-sm',
                 secondary:
-                    'border border-gray-300 bg-white text-gray-900 hover:bg-gray-200',
+                    'border border-gray-300 bg-white text-gray-900 hover:bg-gray-200 shadow-sm',
                 link: 'text-gray-900 underline-offset-4 hover:underline',
                 ghost: 'hover:bg-gray-100 hover:text-gray-900',
             },
