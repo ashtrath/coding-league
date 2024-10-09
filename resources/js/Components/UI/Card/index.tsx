5cc26ade2f3882619eccd5b7c64d39b4eb6fde12
Rahadian Fahri Faizani <69681505+ashtrath@users.noreply.github.com>
2024-10-09 10:26:16 +0700

    feat: add Card components

---
+++ resources/js/Components/UI/Card/index.tsx
@@ -0,0 +1,76 @@
+import { cn } from '@/lib/utils';
+import { type HTMLAttributes } from 'react';
+
+const Card = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <div
+            className={cn(
+                'rounded-xl border border-gray-300 bg-white text-gray-900 shadow-sm',
+                className,
+            )}
+            {...props}
+        />
+    );
+};
+
+const CardHeader = ({
+    className,
+    ...props
+}: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <header
+            className={cn('flex flex-col gap-y-1.5 p-6', className)}
+            {...props}
+        />
+    );
+};
+
+const CardTitle = ({
+    className,
+    ...props
+}: HTMLAttributes<HTMLHeadingElement>) => {
+    return (
+        <h3
+            className={cn(
+                'text-2xl font-semibold leading-none tracking-tight',
+                className,
+            )}
+            {...props}
+        />
+    );
+};
+
+const CardDescription = ({
+    className,
+    ...props
+}: HTMLAttributes<HTMLParagraphElement>) => {
+    return <p className={cn('text-sm text-gray-500', className)} {...props} />;
+};
+
+const CardContent = ({
+    className,
+    ...props
+}: HTMLAttributes<HTMLDivElement>) => {
+    return <div className={cn('p-6 pt-0', className)} {...props} />;
+};
+
+const CardFooter = ({
+    className,
+    ...props
+}: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <footer
+            className={cn('flex items-center p-6 pt-0', className)}
+            {...props}
+        />
+    );
+};
+
+export {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
+};
