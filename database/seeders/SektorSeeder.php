d8f4fc8704742c46c5950b3a256cfaff28ccae87
azuraopal <muhammadnovals334@gmail.com>
2024-10-10 12:08:22 +0700

    fix: typo in sektor

--- database/seeders/SektorSeeder.php
+++ database/seeders/SektorSeeder.php
@@ -2,33 +2,34 @@
 
 namespace Database\Seeders;
 
-use App\Models\Sektor;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Sektor;
 
 class SektorSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Sektor::create([
-            'image' => 'sektor1.jpg',
-            'name' => 'Sektor Pendidikan',
-            'description' => 'Mendukung proyek-proyek yang berhubungan dengan sektor pendidikan.',
-        ]);
+        $sektors = [
+            [
+                'name' => 'Infrastruktur',
+                'image' => 'infrastruktur.jpg',
+                'description' => 'Sektor yang fokus pada pembangunan infrastruktur.'
+            ],
+            [
+                'name' => 'Teknologi',
+                'image' => 'teknologi.jpg',
+                'description' => 'Sektor yang berkaitan dengan pengembangan teknologi.'
+            ],
+            [
+                'name' => 'Pendidikan',
+                'image' => 'pendidikan.jpg',
+                'description' => 'Sektor yang berfokus pada peningkatan kualitas pendidikan.'
+            ],
+        ];
 
-        Sektor::create([
-            'image' => 'sektor2.jpg',
-            'name' => 'Sektor Kesehatan',
-            'description' => 'Proyek yang berfokus pada kesehatan masyarakat.',
-        ]);
-
-        Sektor::create([
-            'image' => 'sektor3.jpg',
-            'name' => 'Sektor Teknologi',
-            'description' => 'Proyek yang melibatkan teknologi dan inovasi.',
-        ]);
+        foreach ($sektors as $sektor) {
+            Sektor::create($sektor);
+        }
     }
 }
