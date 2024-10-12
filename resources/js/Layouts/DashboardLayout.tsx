fb40e1086dac5f95c75271f5c877a3a229e4c7c9
Rahadian Fahri Faizani <69681505+ashtrath@users.noreply.github.com>
2024-10-12 20:56:57 +0700

    feat(DashboardLayout): update styling

--- resources/js/Layouts/DashboardLayout.tsx
+++ resources/js/Layouts/DashboardLayout.tsx
@@ -6,9 +6,9 @@ import { type PropsWithChildren } from 'react';
 const DashboardLayout = ({ children }: PropsWithChildren) => {
     return (
         <>
-            <main className="min-h-[calc(100vh_-_93px)]">
+            <main className="min-h-[calc(100vh_-_93px)] bg-gray-100">
                 <Navbar />
-                <section className="mt-[93px] min-w-full space-y-10 overflow-hidden bg-gray-100 px-24 py-12">
+                <section className="mt-[93px] space-y-10 overflow-hidden px-24 py-12">
                     <Breadcrumbs />
                     <div className="space-y-6">{children}</div>
                 </section>
