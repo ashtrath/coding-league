798f82ac357acdcf91c6ded9c9f074011d8ed170
azuraopal <muhammadnovals334@gmail.com>
2024-10-13 23:13:21 +0700

    feat: add logic for sendMitraNotification and change construct in mitra

--- app/Http/Controllers/Dashboard/DashboardMitraController.php
+++ app/Http/Controllers/Dashboard/DashboardMitraController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Enums\MitraStatus;
+use App\Enums\UserRole;
 use App\Models\Mitra;
 use App\Models\User;
+use App\Notifications\MitraBaruNotification;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
@@ -23,29 +25,40 @@ class DashboardMitraController extends Controller
         return Inertia::render('Dashboard/Mitra/Create');
     }
 
-    public function store(Request $request, User $user)
-    {
-        $validate = $request->validate([
-            'name_mitra' => 'required|string|max:255',
-            'name_company' => 'required|string|max:255',
-            'phone_number' => 'required|string|max:50',
-            'address' => 'required|string',
-        ]);
+        public function store(Request $request, User $user)
+        {
+            $validate = $request->validate([
+                'name_mitra' => 'required|string|max:255',
+                'name_company' => 'required|string|max:255',
+                'phone_number' => 'required|string|max:50',
+                'address' => 'required|string',
+            ]);
 
-        $validate['status'] = MitraStatus::Active;
+            $validate['status'] = MitraStatus::Active;
 
-        Mitra::create($validate);
+            $mitra = Mitra::create($validate);
 
-        if (Auth::check()) {
-            if ($user && $user->first_time_user === 1) {
-                $user->update(['first_time_user' => 0]);
+            if (Auth::check()) {
+                if ($user && $user->first_time_user === 1) {
+                    $user->update(['first_time_user' => 0]);
+                }
+
+                return redirect()->route('dashboard.profile.edit');
+            } else {
+                $this->sendNotification($mitra);
+                return redirect()->route('dashboard.mitra.index')->with('success', 'Mitra Berhasil Dibuat.');
+
             }
-
-            return redirect()->route('dashboard.profile.edit');
-        } else {
-            return redirect()->route('dashboard.mitra.index')->with('success', 'Mitra Berhasil Dibuat.');
         }
-    }
+
+        private function sendNotification(UserRole $mitra)
+        {
+            $adminUsers = User::where('role', UserRole::Admin)->get();
+
+            foreach ($adminUsers as $admin) {
+                $admin->notify(new MitraBaruNotification($mitra));  
+            }
+        }
 
     public function edit(Mitra $mitra)
     {
