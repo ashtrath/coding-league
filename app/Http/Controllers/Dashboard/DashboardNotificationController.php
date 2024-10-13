c2dee42be18f9d6e7c57c0367bf553d1d4134c6e
azuraopal <muhammadnovals334@gmail.com>
2024-10-13 23:30:22 +0700

    feat: logic markAsRead for dashboard notification

---
+++ app/Http/Controllers/Dashboard/DashboardNotificationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class DashboardNotificationController extends Controller
+{
+    public function index(Request $request)
+    {
+        $notifications = Auth::user()->notifications;
+
+        return response()->json($notifications);
+    }
+
+    public function markAsRead(Request $request)
+    {
+        $request->user()->unreadNotifications->markAsRead();
+
+        return response()->json(['message' => 'Notifikasi berhasil ditandai sebagai dibaca.']);
+    }
+}
