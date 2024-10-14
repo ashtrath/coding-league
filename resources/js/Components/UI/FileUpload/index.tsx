90b8591078b024ef0633927666669755e4560999
Rahadian Fahri Faizani <69681505+ashtrath@users.noreply.github.com>
2024-10-14 18:22:01 +0700

    feat: use ArkUI file-upload

--- resources/js/Components/UI/FileUpload/index.tsx
+++ resources/js/Components/UI/FileUpload/index.tsx
@@ -1,127 +1,126 @@
 import { cn } from '@/lib/utils';
-import { RiUploadCloud2Line } from '@remixicon/react';
-import { useEffect, useState } from 'react';
-import { type DropzoneProps, useDropzone } from 'react-dropzone';
+import { FileUpload as FileUploadPrimitive } from '@ark-ui/react/file-upload';
 
-interface FileUploadProps extends DropzoneProps {
-    className?: string;
-    onFilesUploaded: (files: File[]) => void;
-}
+const FileUpload = FileUploadPrimitive.Root;
 
-const FileUpload = ({
-    onFilesUploaded,
-    maxFiles = 1,
+const FileUploadTrigger = FileUploadPrimitive.Trigger;
+
+const FileUploadHiddenInput = FileUploadPrimitive.HiddenInput;
+
+const FileUploadItemPreview = FileUploadPrimitive.ItemPreview;
+
+const FileUploadItemDeleteTrigger = FileUploadPrimitive.ItemDeleteTrigger;
+
+const FileUploadLabel = ({
     className,
     ...props
-}: FileUploadProps) => {
-    const [previews, setPreviews] = useState<string[]>([]);
-
-    const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
-        useDropzone({
-            onDrop: (acceptedFiles: File[]) => {
-                onFilesUploaded(acceptedFiles);
-
-                const preview = acceptedFiles.map((file) =>
-                    URL.createObjectURL(file),
-                );
-                setPreviews(preview);
-            },
-            ...props,
-        });
-
-    const acceptedFormats = props.accept
-        ? Object.values(props.accept)
-              .flat()
-              .map((ext) => ext.replace('.', '').toUpperCase())
-              .join(', ')
-        : 'Any';
-
-    useEffect(() => {
-        return () => previews.forEach((url) => URL.revokeObjectURL(url));
-    }, [previews]);
-
+}: FileUploadPrimitive.LabelProps) => {
     return (
-        <div
+        <FileUploadPrimitive.Label
             className={cn(
-                'flex cursor-pointer flex-col justify-center space-y-3 rounded-lg border border-gray-300 p-4 pb-6',
-                isDragActive ? 'bg-gray-100' : 'bg-white',
+                'text-sm font-medium text-gray-600 peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
                 className,
             )}
-            {...getRootProps()}
-        >
-            <input {...getInputProps()} />
-            {previews.length > 0 ? (
-                <div
-                    className={cn(
-                        'grid',
-                        maxFiles > 1 ? 'grid-cols-3' : 'grid-cols-1',
-                    )}
-                >
-                    {previews.map((preview, index) => (
-                        <div key={index} className="relative size-72">
-                            <img
-                                src={preview}
-                                alt={`Preview ${index}`}
-                                onLoad={() => URL.revokeObjectURL(preview)}
-                                className="size-72 rounded-lg object-contain"
-                            />
-                            <div className="absolute inset-0 z-10 flex flex-col justify-end rounded-lg bg-gradient-to-t from-black/70 to-transparent p-2">
-                                <p className="bottom-0 text-sm font-semibold text-white">
-                                    {acceptedFiles[index].name}
-                                </p>
-                                <p className="bottom-0 text-sm font-semibold text-white">
-                                    {(
-                                        acceptedFiles[index].size /
-                                        (1024 * 1024)
-                                    ).toFixed(2)}{' '}
-                                    MB
-                                </p>
-                            </div>
-                        </div>
-                    ))}
-                    {/* {maxFiles > 1 && (
-                        <div className="grid size-72 place-content-center place-items-center rounded-lg border-2 border-dashed border-brand">
-                            <div className="size-fit self-center rounded-full border-[6px] border-red-100 bg-red-200 p-2 text-brand">
-                                <RiUploadCloud2Line className="size-5" />
-                            </div>
-                            <div className="space-y-1 text-center text-sm font-medium text-gray-600">
-                                <p>
-                                    <span className="font-semibold text-brand">
-                                        Klik untuk unggah
-                                    </span>{' '}
-                                    atau seret dan lepas disini
-                                </p>
-                                <p>
-                                    {acceptedFormats}{' '}
-                                    {props.maxSize &&
-                                        `up to ${(props.maxSize / (1024 * 1024)).toFixed(0)} MB`}
-                                </p>
-                            </div>
-                        </div>
-                    )} */}
-                </div>
-            ) : (
-                <>
-                    <div className="size-fit self-center rounded-full border-[6px] border-red-100 bg-red-200 p-2 text-brand">
-                        <RiUploadCloud2Line className="size-5" />
-                    </div>
-                    <div className="space-y-1 text-center text-sm font-medium text-gray-600">
-                        <p>
-                            <span className="font-semibold text-brand">
-                                Klik untuk unggah
-                            </span>{' '}
-                            atau seret dan lepas disini
-                        </p>
-                        <p>
-                            {acceptedFormats}{' '}
-                            {props.maxSize &&
-                                `up to ${(props.maxSize / (1024 * 1024)).toFixed(0)} MB`}
-                        </p>
-                    </div>
-                </>
-            )}
-        </div>
+            {...props}
+        />
     );
 };
 
-export default FileUpload;
+const FileUploadDropzone = ({
+    className,
+    ...props
+}: FileUploadPrimitive.DropzoneProps) => {
+    return (
+        <FileUploadPrimitive.Dropzone
+            className={cn(
+                'flex flex-col items-center justify-center gap-3 rounded-lg border border-gray-300 bg-white px-3 py-6 pt-5 shadow-sm',
+                className,
+            )}
+            {...props}
+        />
+    );
+};
+
+const FileUploadItemGroup = ({
+    className,
+    children,
+    ...props
+}: Omit<FileUploadPrimitive.ItemGroupProps, 'children'> &
+    FileUploadPrimitive.ContextProps) => {
+    return (
+        <FileUploadPrimitive.ItemGroup
+            className={cn('flex flex-col gap-3', className)}
+            {...props}
+        >
+            <FileUploadPrimitive.Context>
+                {children}
+            </FileUploadPrimitive.Context>
+        </FileUploadPrimitive.ItemGroup>
+    );
+};
+
+const FileUploadItem = ({
+    className,
+    ...props
+}: FileUploadPrimitive.ItemProps) => {
+    return (
+        <FileUploadPrimitive.Item
+            className={cn(
+                'flex items-center gap-x-3 rounded-xl border border-gray-300 bg-white p-4 shadow-sm animate-in fade-in-5',
+                className,
+            )}
+            {...props}
+        />
+    );
+};
+
+const FileUploadItemPreviewImage = ({
+    className,
+    ...props
+}: FileUploadPrimitive.ItemPreviewImageProps) => {
+    return (
+        <FileUploadPrimitive.ItemPreviewImage
+            className={cn('aspect-square size-16 object-scale-down', className)}
+            {...props}
+        />
+    );
+};
+
+const FileUploadItemName = ({
+    className,
+    ...props
+}: FileUploadPrimitive.ItemNameProps) => {
+    return (
+        <FileUploadPrimitive.ItemName
+            className={cn('text-sm font-semibold text-gray-900', className)}
+            {...props}
+        />
+    );
+};
+
+const FileUploadItemSizeText = ({
+    className,
+    ...props
+}: FileUploadPrimitive.ItemSizeTextProps) => {
+    return (
+        <FileUploadPrimitive.ItemSizeText
+            className={cn('text-sm text-gray-900', className)}
+            {...props}
+        />
+    );
+};
+
+export {
+    FileUpload,
+    FileUploadDropzone,
+    FileUploadHiddenInput,
+    FileUploadItem,
+    FileUploadItemDeleteTrigger,
+    FileUploadItemGroup,
+    FileUploadItemName,
+    FileUploadItemPreview,
+    FileUploadItemPreviewImage,
+    FileUploadItemSizeText,
+    FileUploadLabel,
+    FileUploadTrigger,
+};
