70b6b8c7985e7e10dc64cc14d9cbf16010d50d55
azuraopal <muhammadnovals334@gmail.com>
2024-10-18 13:25:55 +0700

    feat: add table pivot kegiatan_tags and laporan_tags

---
+++ database/migrations/2024_10_18_062049_create_laporan_tag_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('laporan_tag', function (Blueprint $table) {
+            $table->ulid('id');
+            $table->foreignUlid('laporan_id')->constrained()->onDelete('cascade');
+            $table->foreignUlid('tag_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laporan_tag');
+    }
+};
