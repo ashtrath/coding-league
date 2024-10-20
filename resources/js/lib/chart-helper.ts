46f25b03b7f593506516fb81dc4d2526e3391108
Rahadian Fahri Faizani <69681505+ashtrath@users.noreply.github.com>
2024-10-20 14:34:35 +0700

    feat(chart-helper): update createChartConfig paramerter type definition

--- resources/js/lib/chart-helper.ts
+++ resources/js/lib/chart-helper.ts
@@ -1,16 +1,15 @@
 import { ChartConfig } from '@/Components/UI/Chart';
 
-export const createChartConfig = <T>(
+export const createChartConfig = <T extends Record<string, any>>(
     data: T[],
     nameKey: keyof T,
-    dataKey: keyof T,
 ) => {
     const chartConfig: ChartConfig = {};
 
     data.forEach((item, index) => {
         chartConfig[String(item[nameKey])] = {
             label: String(item[nameKey]),
-            color: `hsl(var(--chart-${index + 1}))`,
+            color: `var(--chart-${index + 1})`,
         };
     });
 
