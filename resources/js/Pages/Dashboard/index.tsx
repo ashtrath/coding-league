af5e5c7dd97681b72108cf3335cdfce24a504840
Rahadian Fahri Faizani <69681505+ashtrath@users.noreply.github.com>
2024-10-20 14:35:45 +0700

    chore(Dashboard): update import

--- resources/js/Pages/Dashboard/index.tsx
+++ resources/js/Pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
 import StatisticsCard from '@/Components/Card/StatisticsCard';
-import PersentasiAnggaranSektorCSRChart from '@/Components/Chart/PersentasiAnggaranSektorCSRChart';
-import RealisasiKecamatanCSR from '@/Components/Chart/RealisasiKecamatanCSR';
-import RealisasiPTCSR from '@/Components/Chart/RealisasiPTCSR';
-import RealisasiSektorCSR from '@/Components/Chart/RealisasiSektorCSR';
+import PersentaseAnggaranBySektorChart from '@/Components/Chart/PersentaseAnggaranBySektorChart';
+import TotalRealisasiByKecamatanChart from '@/Components/Chart/TotalRealisasiByKecamatanChart';
+import TotalRealisasiByMitraChart from '@/Components/Chart/TotalRealisasiByMitraChart';
+import TotalRealisasiBySektorChart from '@/Components/Chart/TotalRealisasiBySektorChart';
 import { Card, CardContent } from '@/Components/UI/Card';
 import MainDashboardLayout from '@/Layouts/MainDashboardLayout';
 import { formatCurrency } from '@/lib/utils';
@@ -44,8 +44,8 @@ interface DashboardAnalytics {
 export default function Dashboard({
     data,
 }: PageProps<{ data: DashboardAnalytics }>) {
-    console.log(data);
     const { analytics, chartData } = data;
+
     return (
         <MainDashboardLayout>
             <Head title="Dashboard" />
@@ -97,7 +97,7 @@ export default function Dashboard({
                                 Persentase Total Realisasi Berdasarkan Sektor
                                 CSR
                             </h3>
-                            <PersentasiAnggaranSektorCSRChart
+                            <PersentaseAnggaranBySektorChart
                                 chartData={chartData.sektor}
                             />
                         </div>
@@ -105,19 +105,23 @@ export default function Dashboard({
                             <h3 className="whitespace-nowrap text-[22px] font-semibold leading-none tracking-tight">
                                 Total Realisasi Sektor CSR
                             </h3>
-                            <RealisasiSektorCSR chartData={chartData.sektor} />
+                            <TotalRealisasiBySektorChart
+                                chartData={chartData.sektor}
+                            />
                         </div>
                         <div className="space-y-5">
                             <h3 className="whitespace-nowrap text-[22px] font-semibold leading-none tracking-tight">
                                 Persentase Total Realisasi Berdasarkan PT
                             </h3>
-                            <RealisasiPTCSR chartData={chartData.mitra} />
+                            <TotalRealisasiByMitraChart
+                                chartData={chartData.mitra}
+                            />
                         </div>
                         <div className="space-y-5">
                             <h3 className="whitespace-nowrap text-[22px] font-semibold leading-none tracking-tight">
                                 Persentase Total Realisasi Berdasarkan Kecamatan
                             </h3>
-                            <RealisasiKecamatanCSR
+                            <TotalRealisasiByKecamatanChart
                                 chartData={chartData.kecamatan}
                             />
                         </div>
